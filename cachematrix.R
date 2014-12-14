60cd98b53a1a13920cf92ce22c58281d3373a91b
Tracy Pham <tracypham305@gmail.com>
2014-12-14 17:20:14 +0800

    v1

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
@@ -12,4 +23,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+       data <- x$get()
+        m <- x$getinverse()
+       if(identical(x, data) && !is.null(m)) {
+                message("getting cached data")
+                return(m)
+       }
+       
+       ## Calculate inverse
+       ## if x is square matrix, use solve()
+       ## else use ginv() in the MASS package - with the assumption input matrix is inversible
+       if(dim(data)[1] == dim(data)[2]){
+       	m <- solve(data, ...)	
+       }else{
+       	m <- ginv(x)
+       }
+        
+        x$set(x)
+        x$setinverse(m)
+        m
+
 }
